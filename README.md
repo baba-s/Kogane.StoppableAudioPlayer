ac0ceb2d3a7db14f0171e24a18b3490ea875bb3d
baba-s <shotababa1206@gmail.com>
2022-10-31 13:45:43 +0900

    add

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Kogane.StoppableAudioPlayer
+# Kogane Stoppable Audio Player
+
+再生中の AudioClip を停止できるコンポーネント
---
+++ Runtime/StoppableAudioPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kogane
+{
+    [DisallowMultipleComponent]
+    public sealed class StoppableAudioPlayer : MonoBehaviour
+    {
+        [SerializeField] private AudioSource[] m_audioSources;
+
+        public IDisposable Play( AudioClip audioClip )
+        {
+            return Play( audioClip, null );
+        }
+
+        public IDisposable Play( AudioClip audioClip, float? volume )
+        {
+            foreach ( var audioSource in m_audioSources )
+            {
+                if ( audioSource.isPlaying ) continue;
+
+                audioSource.clip = audioClip;
+
+                if ( volume != null )
+                {
+                    audioSource.volume = ( float )volume;
+                }
+
+                audioSource.Play();
+
+                return Disposable.Create( () => Stop( audioClip ) );
+            }
+
+            return Disposable.Empty;
+        }
+
+        public void Stop( AudioClip audioClip )
+        {
+            foreach ( var audioSource in m_audioSources )
+            {
+                if ( audioSource.clip != audioClip ) continue;
+                audioSource.Stop();
+            }
+        }
+    }
+}
